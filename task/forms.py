c4e5ad849d9961186e01586de2f2e2a903af3bbf
renuka987 <119907422+renuka987@users.noreply.github.com>
2025-02-13 11:52:56 +0545

    implemented task transfer when an employee is reassigned to different manager

--- task/forms.py
+++ task/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.models import inlineformset_factory
-from .models import Task, SubTask
+from .models import Task, SubTask, User
 
 
 class TaskForm(forms.ModelForm):
@@ -9,19 +9,30 @@ class TaskForm(forms.ModelForm):
         fields = ["title", "description", "due_date", "status", "assigned_to"]
 
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)  # Get the logged-in user from kwargs
         super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
+
+        if user:
+            if user.role == User.Role_Type.ADMIN:
+                self.fields["assigned_to"].queryset = User.objects.all()
+            elif user.role == User.Role_Type.MANAGER:
+                self.fields["assigned_to"].queryset = User.objects.filter(
+                    report_to=user
+                )
+
+        for field in self.fields.values():
             field.widget.attrs.update({"class": "form-control"})
 
 
+# Define SubTaskForm before using it in the formset
 class SubTaskForm(forms.ModelForm):
     class Meta:
         model = SubTask
-        fields = ["title", "description", "due_date", "status"]
+        fields = ["title", "description", "status"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
+        for field in self.fields.values():
             field.widget.attrs.update({"class": "form-control"})
 
 
@@ -29,13 +40,13 @@ class SubTaskForm(forms.ModelForm):
 SubTaskFormSet = inlineformset_factory(
     Task,
     SubTask,
-    form=SubTaskForm,
+    form=SubTaskForm,  # Now SubTaskForm is properly defined
     extra=0,  # Number of extra empty forms to display
     can_delete=True,  # Allow deleting sub-tasks
 )
 
 
-class TaskStatusForm(forms.ModelForm): 
+class TaskStatusForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["status"]
