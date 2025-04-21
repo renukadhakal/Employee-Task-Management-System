1bb2823e16a9bda27e35f3215450ef29a1fc99ad
renuka987 <119907422+renuka987@users.noreply.github.com>
2025-04-21 16:44:39 +0545

    fixed online-offline mode in chat feature

--- chat/views.py
+++ chat/views.py
@@ -25,7 +25,10 @@ def chat_view(request, username):
     messages = Message.objects.filter(
         sender__in=[request.user, other_user], receiver__in=[request.user, other_user]
     )
-
+    last_seen = cache.get(f"online-{other_user.username}")
+    is_online = last_seen and datetime.now() - last_seen < timedelta(seconds=60)
     return render(
-        request, "chat/chat.html", {"other_user": other_user, "messages": messages}
+        request,
+        "chat/chat.html",
+        {"other_user": other_user, "messages": messages, "is_online": is_online},
     )
